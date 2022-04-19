3a62a6e45c36dad5879bedc8e89355fca6a1616b
Dylan Koevort <218088159@mycput.ac.za>
2022-04-19 19:49:44 +0200

    added post

--- src/Post.js
+++ src/Post.js
@@ -3,6 +3,10 @@ import { Avatar } from '@mui/material';
 import './Post.css';
 import avatarIcon from './avatar.png';
 import VerifiedIcon from '@mui/icons-material/Verified';
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import IosShareIcon from '@mui/icons-material/IosShare';
 
 function Post({displayName, username, verified, text, image, avatar}) {
   return (
@@ -20,6 +24,12 @@ function Post({displayName, username, verified, text, image, avatar}) {
               </div>
             </div>
             <img src="https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif" alt='React Gif'/>
+            <div className='post_footer'>
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <IosShareIcon fontSize="small" />
+            </div>
         </div>
     </div>
   )
